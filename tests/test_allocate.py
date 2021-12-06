fd5ce0de00432ecc1d1f12872e52e51191817d5d
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2021-12-06 15:22:30 +0900

    more extensive tests

--- tests/test_allocate.py
+++ tests/test_allocate.py
@@ -1,5 +1,7 @@
 import unittest
 
+import numpy as np
+
 import pymemalign
 
 
@@ -10,9 +12,101 @@ class TestAllocation(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_allocate(self):
-        """Test memory allocation."""
+    def __test_allocate(self, shape, dtypes, expected_type):
+        for t in dtypes:
+            arr = pymemalign.empty_aligned(shape=shape, dtype=t)
+            print(f'expected shape {shape}, actual shape {arr.shape}')
+            print(f'input type {t}, output type {arr.dtype}')
+            self.assertEqual(shape, arr.shape)
+            self.assertEqual(expected_type, arr.dtype)
+
+    def test_allocate_int8(self):
+        """Test memory allocation (int8)."""
         shape = (10, 10)
-        arr = pymemalign.empty_aligned(shape=shape)
-        print(f'expected shape {shape}, actual shape {arr.shape}')
-        self.assertEqual(shape, arr.shape)
+        expected = np.int8
+        types_tested = [np.int8, np.byte]
+        self.__test_allocate(shape, types_tested, expected)
+
+    def test_allocate_int16(self):
+        """Test memory allocation (int16)."""
+        shape = (10, 10)
+        expected = np.int16
+        types_tested = [np.int16, np.short]
+        self.__test_allocate(shape, types_tested, expected)
+
+    def test_allocate_int32(self):
+        """Test memory allocation (int32)."""
+        shape = (10, 10)
+        expected = np.int32
+        types_tested = [np.int32]
+        self.__test_allocate(shape, types_tested, expected)
+
+    def test_allocate_int64(self):
+        """Test memory allocation (int64)."""
+        shape = (10, 10)
+        expected = np.int64
+        types_tested = [int, np.int, np.int64, np.long, np.longlong]
+        self.__test_allocate(shape, types_tested, expected)
+
+    def test_allocate_uint8(self):
+        """Test memory allocation (uint8)."""
+        shape = (10, 10)
+        expected = np.uint8
+        types_tested = [np.uint8, np.ubyte]
+        self.__test_allocate(shape, types_tested, expected)
+
+    def test_allocate_uint16(self):
+        """Test memory allocation (uint16)."""
+        shape = (10, 10)
+        expected = np.uint16
+        types_tested = [np.uint16, np.ushort]
+        self.__test_allocate(shape, types_tested, expected)
+
+    def test_allocate_uint32(self):
+        """Test memory allocation (uint32)."""
+        shape = (10, 10)
+        expected = np.uint32
+        types_tested = [np.uint32, np.uint]
+        self.__test_allocate(shape, types_tested, expected)
+
+    def test_allocate_uint64(self):
+        """Test memory allocation (uint64)."""
+        shape = (10, 10)
+        expected = np.uint32
+        types_tested = [np.uint64, np.ulonglong]
+        self.__test_allocate(shape, types_tested, expected)
+
+    def test_allocate_float16(self):
+        """Test memory allocation (float16)."""
+        shape = (10, 10)
+        expected = np.float16
+        types_tested = [np.half, np.float16]
+        self.__test_allocate(shape, types_tested, expected)
+
+    def test_allocate_float32(self):
+        """Test memory allocation (float32)."""
+        shape = (10, 10)
+        expected = np.float32
+        types_tested = [float, np.float, np.float32]
+        self.__test_allocate(shape, types_tested, expected)
+
+    def test_allocate_float64(self):
+        """Test memory allocation (float64)."""
+        shape = (10, 10)
+        expected = np.float64
+        types_tested = [np.double, np.float64]
+        self.__test_allocate(shape, types_tested, expected)
+
+    def test_allocate_complex64(self):
+        """Test memory allocation (complex64)."""
+        shape = (10, 10)
+        expected = np.complex64
+        types_tested = [complex, np.complex, np.complex64]
+        self.__test_allocate(shape, types_tested, expected)
+
+    def test_allocate_complex128(self):
+        """Test memory allocation (complex128)."""
+        shape = (10, 10)
+        expected = np.complex128
+        types_tested = [np.complex128]
+        self.__test_allocate(shape, types_tested, expected)
